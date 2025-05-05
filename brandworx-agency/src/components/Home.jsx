25bc9fb2bee6f6fde529e91d9a2291c7d02ea6ec
naveed69 <naveed.desai.69@gmail.com>
2025-05-06 02:52:46 +0530

    created landing page

--- brandworx-agency/src/components/Home.jsx
+++ brandworx-agency/src/components/Home.jsx
@@ -4,7 +4,37 @@ export const Home = () => {
   return (
     <div className="hero-section">
       <Navbar />
-      <h1>Hello Home</h1>
+      <div className="content-hero">
+        <span className="social-media">
+          <div class="fa cursor">&#xf16d;</div>
+          <div class="fa cursor">&#xf232;</div>
+          <div class="fa cursor">&#xf08c;</div>
+          <div class="fa cursor">&#xf099;</div>
+        </span>
+        <div
+          className="hero-name-container"
+          style={{
+            width: "1100px",
+            height: "220px",
+          }}
+        >
+          <p className="hero-name">
+            Amazing Digital Creations with{" "}
+            <span
+              style={{
+                color: "#E4E800",
+              }}
+            >
+              Brandworx agency
+            </span>
+          </p>
+          <p className="help-section">
+            Help You To Build Digital Solutions That is Modern, User Friendly,
+            Good CEO, and Clean Design
+          </p>
+          <button className="get-Started-btn cursor">Get Sarted</button>
+        </div>
+      </div>
     </div>
   );
 };
--- brandworx-agency/src/components/NavBar/Navbar.jsx
+++ brandworx-agency/src/components/NavBar/Navbar.jsx
@@ -2,9 +2,72 @@ import "./Navbar.css";
 export const Navbar = () => {
   return (
     <header className="navbar-container">
-      <div>navbar</div>
-      <div>navbar</div>
-      <div>navbar</div>
+      <div className="brand-name">
+        <div className="logo"></div>
+        <p
+          style={{
+            fontSize: "16.2px",
+            fontFamily: "Poppins",
+            marginLeft: "15px",
+            color:
+              "background: linear-gradient(333.6deg, #999999 21.9%, #F2FF00 97.42%)",
+            fontWeight: "700",
+          }}
+        >
+          Brandworx
+        </p>
+      </div>
+      <div className="navbar">
+        <a
+          href="#home"
+          style={{
+            color: "white",
+            textDecoration: "none",
+          }}
+        >
+          Home
+        </a>
+        <a
+          href="#about"
+          style={{
+            color: "white",
+            textDecoration: "none",
+          }}
+        >
+          About
+        </a>
+        <a
+          href="#services"
+          style={{
+            color: "white",
+            textDecoration: "none",
+          }}
+        >
+          Services
+        </a>
+        <a
+          href="#portfolio"
+          style={{
+            color: "white",
+            textDecoration: "none",
+          }}
+        >
+          Portfolio
+        </a>
+        <a
+          href="#contactus"
+          style={{
+            color: "white",
+            textDecoration: "none",
+          }}
+        >
+          Contact Us
+        </a>
+      </div>
+      <div className="contact-navbar">
+        <p>Contact Us</p>
+        <span className="contact-arrow">&#8599;</span>
+      </div>
     </header>
   );
 };
