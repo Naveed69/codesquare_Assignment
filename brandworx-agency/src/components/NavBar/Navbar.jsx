52cd29339ea963904ce608e3e7e7d60428266aae
naveed69 <naveed.desai.69@gmail.com>
2025-05-06 13:03:27 +0530

    mobile view

--- brandworx-agency/src/components/NavBar/Navbar.jsx
+++ brandworx-agency/src/components/NavBar/Navbar.jsx
@@ -1,28 +1,48 @@
+import { useState, useEffect } from "react";
 import "./Navbar.css";
 export const Navbar = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // Update state on window resize
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup on component unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
   return (
     <header className="navbar-container">
       <div className="brand-name">
         <div className="logo"></div>
-        <p
-          style={{
-            fontSize: "16.2px",
-            fontFamily: "Poppins",
-            marginLeft: "25px",
-            color:
-              "background: linear-gradient(333.6deg, #999999 21.9%, #F2FF00 97.42%)",
-            fontWeight: "700",
-          }}
-        >
-          Brandworx
-        </p>
+        {windowWidth > 768 && (
+          <p
+            style={{
+              fontSize: "16.2px",
+              fontFamily: "Poppins",
+              marginLeft: "25px",
+              color:
+                "background: linear-gradient(333.6deg, #999999 21.9%, #F2FF00 97.42%)",
+              fontWeight: "700",
+            }}
+          >
+            Brandworx
+          </p>
+        )}
       </div>
 
-      <nav class="navbar">
-        <button class="menu-toggle" onclick="toggleMenu()">
+      <nav className="navbar">
+        <button className="menu-toggle cursor" onClick={toggleMenu}>
           ☰
         </button>
-        <div className="navbar nav-links" id="navLinks">
+        <div className={`nav-links ${menuOpen ? "active" : ""}`}>
           <a href="#home">Home</a>
           <a href="#about">About</a>
           <a href="#services">Services</a>
@@ -30,10 +50,12 @@ export const Navbar = () => {
           <a href="#contactus">Contact Us</a>
         </div>
       </nav>
-      <div className="contact-navbar">
-        <p>Contact Us</p>
-        <span className="contact-arrow">&#8599;</span>
-      </div>
+      {windowWidth > 768 && (
+        <div className="contact-navbar">
+          <p>Contact Us</p>
+          <span className="contact-arrow">&#8599;</span>
+        </div>
+      )}
     </header>
   );
 };
